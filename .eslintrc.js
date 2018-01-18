5610e0d93325a8e26dd2888827a55ca29eb5e53c
Gaya Kessler <gaya.kessler@gmail.com>
2018-01-18 07:56:18 +0100

    mark 'to' as valid for anchors

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,10 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
     'react/require-default-props': 0,
     'import/prefer-default-export': 0,
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['to'],
+      aspects: ['noHref', 'invalidHref', 'preferButton'],
+    }],
   },
 };
