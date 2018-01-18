deef24f264c6f7fa9f3d975ae67c6a19fb26701c
Gaya Kessler <gaya.kessler@gmail.com>
2018-01-18 17:59:12 +0100

    sort time by date and start

--- src/pages/Time.js
+++ src/pages/Time.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import isThisWeek from 'date-fns/is_this_week';
+import isAfter from 'date-fns/is_after';
+import isBefore from 'date-fns/is_before';
 
 import ThymeTable from '../components/ThymeTable';
 import NewTime from '../components/ThymeTable/New';
@@ -10,10 +13,15 @@ type TimeType = {
   entries: Array<timeType>,
 };
 
+function thisWeek(entries) {
+  return entries.filter(entry => isThisWeek(entry.date));
+}
+
 function Time({ entries }: TimeType) {
   return (
     <div>
-      <ThymeTable entries={entries} />
+      <h4>This week</h4>
+      <ThymeTable entries={thisWeek(entries)} />
       <NewTime />
     </div>
   );
@@ -22,6 +30,21 @@ function Time({ entries }: TimeType) {
 function mapStateToProps(state) {
   const { allIds, byId } = state.time;
   const entries = allIds.map(id => byId[id]);
+
+  entries.sort((a, b) => {
+    const aDate = `${a.date} ${a.start}`;
+    const bDate = `${b.date} ${b.start}`;
+
+    if (isBefore(aDate, bDate)) {
+      return -1;
+    }
+
+    if (isAfter(aDate, bDate)) {
+      return 1;
+    }
+
+    return 0;
+  });
 
   return { entries };
 }
