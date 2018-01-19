7f869d56b606c169417ebd49e730cf97af2f6d73
Gaya Kessler <gaya.kessler@gmail.com>
2018-01-19 16:07:13 +0100

    setup fonts

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -22,6 +22,7 @@ function Reports({ projects }) {
         </div>
       </div>
       <div className="Report__filters">
+        <span className="Report__filters-label">Show:</span>
         {sortedProjects(projects).map(project => (
           <label className="Report__filter" key={project.id} htmlFor={project.id}>
             <span className="Report__filter-name">{project.name}</span>
