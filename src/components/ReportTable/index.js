a9c7790e602b11c7575ec894a13b11b11b7b250c
Gaya Kessler <gaya.kessler@gmail.com>
2018-01-23 16:55:25 +0100

    format total time in minutes

--- src/components/ReportTable/index.js
+++ src/components/ReportTable/index.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import { formatDuration } from '../../core/thyme';
+
 import './ReportTable.css';
 
 type ReportTableType = {
@@ -14,18 +16,18 @@ function ReportTable({ projects }: ReportTableType) {
       <tbody>
         <tr className="ReportTable__header">
           <th>Project</th>
-          <th className="ReportTable__total-header">Total hours</th>
+          <th className="ReportTable__total-header">Total spent</th>
         </tr>
         {projects.map(project => (
           <tr key={project.id}>
             <td>{project.nameTree.join(' > ')}</td>
-            <td>{project.time}</td>
+            <td className="ReportTable__duration">{formatDuration(project.time * 60)}</td>
           </tr>
         ))}
         <tr className="ReportTable__total">
           <td />
-          <td>
-            {projects.reduce((total, project) => total + project.time, 0)}
+          <td className="ReportTable__duration">
+            {formatDuration(projects.reduce((total, project) => total + (project.time * 60), 0))}
           </td>
         </tr>
       </tbody>
