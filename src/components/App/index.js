750bf535cba592db3fd29ec9a307a859fae3d99f
Gaya Kessler <gaya.kessler@gmail.com>
2018-01-24 20:24:55 +0100

    make container fluid

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -39,7 +39,7 @@ function App({ location, children }: AppType) {
           {AppLink('Settings', '/settings', location.pathname)}
         </Container>
       </Menu>
-      <Container style={{ marginTop: '6em' }}>{children}</Container>
+      <Container fluid style={{ marginTop: '6em' }}>{children}</Container>
     </div>
   );
 }
