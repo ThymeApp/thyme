965e50db7c8fd0b5c186f2211623cbf0d74d859f
Gaya Kessler <gaya.kessler@gmail.com>
2018-02-23 13:47:40 +0100

    add action to add report

--- src/actions/reports.js
+++ src/actions/reports.js
@@ -1,5 +1,7 @@
 // @flow
 
+import shortid from 'shortid';
+
 export function resetFilters(filters: Array<string>) {
   return {
     type: 'RESET_FILTERS',
@@ -17,6 +19,17 @@ export function toggleFilter(filter: string | null) {
 export function updateDateRange(from: Date, to: Date) {
   return {
     type: 'UPDATE_DATE_RANGE',
+    from,
+    to,
+  };
+}
+
+export function addReport(name: string, filters: Array<string>, from: Date, to: Date) {
+  return {
+    type: 'ADD_REPORT',
+    id: shortid.generate(),
+    name,
+    filters,
     from,
     to,
   };
