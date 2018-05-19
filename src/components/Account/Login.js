0ef2691c8924410d675a1b16e1350346e7211c25
Gaya Kessler <gaya.kessler@gmail.com>
2018-05-19 10:01:47 +0200

    refactor login to own component

---
+++ src/components/Account/Login.js
@@ -0,0 +1,76 @@
+// @flow
+import React, { Component } from 'react';
+
+import Form from 'semantic-ui-react/dist/commonjs/collections/Form';
+import Input from 'semantic-ui-react/dist/commonjs/elements/Input';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+
+type LoginState = {
+  email: string,
+  password: string,
+  isLoading: boolean,
+};
+
+class Login extends Component<*, LoginState> {
+  state = {
+    email: '',
+    password: '',
+    isLoading: false,
+  };
+
+  onSubmit = () => {
+    this.setState({ isLoading: true });
+  };
+
+  handleInput = (e: SyntheticEvent<HTMLButtonElement>) => {
+    if (e.target instanceof HTMLInputElement) {
+      this.setState({
+        [e.currentTarget.name]: e.currentTarget.value,
+      });
+    }
+  };
+
+  render() {
+    const { email, password, isLoading } = this.state;
+
+    return (
+      <Form
+        style={{ width: 230 }}
+        loading={isLoading}
+        onSubmit={this.onSubmit}
+      >
+        <Form.Field>
+          <label htmlFor="email">Email address</label>
+          <Input
+            id="email"
+            type="text"
+            size="small"
+            name="email"
+            value={email}
+            onChange={this.handleInput}
+            placeholder="Your email address"
+          />
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="password">Password</label>
+          <Input
+            id="password"
+            type="password"
+            size="small"
+            name="password"
+            value={password}
+            onChange={this.handleInput}
+            placeholder="Your password"
+          />
+        </Form.Field>
+        <section className="Account__Submit-Bar">
+          <Form.Button primary>Login</Form.Button>
+          or
+          <Button basic>Register</Button>
+        </section>
+      </Form>
+    );
+  }
+}
+
+export default Login;
--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -3,58 +3,23 @@ import React, { Component } from 'react';
 
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 import Popup from 'semantic-ui-react/dist/commonjs/modules/Popup';
-import Form from 'semantic-ui-react/dist/commonjs/collections/Form';
-import Input from 'semantic-ui-react/dist/commonjs/elements/Input';
+
+import Login from './Login';
 
 import './style.css';
 
 type AccountState = {
   isOpen: boolean,
-  isLoading: boolean,
-  login: {
-    email: string,
-    password: string,
-  },
 }
 
 class Account extends Component<*, AccountState> {
-  state = {
-    isOpen: false,
-    isLoading: false,
-    login: {
-      email: '',
-      password: '',
-    },
-  };
-
-  onSubmit = () => {
-    const { login } = this.state;
-
-    this.setState({ isLoading: true });
-
-    console.log(login.email, login.password);
-  };
+  state = { isOpen: false };
 
   handleOpen = () => this.setState({ isOpen: true });
   handleClose = () => this.setState({ isOpen: false });
 
-  handleInput = (e: SyntheticEvent<HTMLButtonElement>) => {
-    if (e.target instanceof HTMLInputElement) {
-      const [obj, prop] = e.currentTarget.name.split('.');
-
-      if (obj && prop) {
-        this.setState({
-          [obj]: {
-            ...this.state[obj],
-            [prop]: e.currentTarget.value || '',
-          },
-        });
-      }
-    }
-  };
-
   render() {
-    const { isOpen, isLoading, login } = this.state;
+    const { isOpen } = this.state;
 
     return (
       <div>
@@ -66,41 +31,7 @@ class Account extends Component<*, AccountState> {
           onClose={this.handleClose}
           onOpen={this.handleOpen}
         >
-          <Form
-            style={{ width: 230 }}
-            loading={isLoading}
-            onSubmit={this.onSubmit}
-          >
-            <Form.Field>
-              <label htmlFor="login.email">Email address</label>
-              <Input
-                id="login.email"
-                type="text"
-                size="small"
-                name="login.email"
-                value={login.email}
-                onChange={this.handleInput}
-                placeholder="Your email address"
-              />
-            </Form.Field>
-            <Form.Field>
-              <label htmlFor="login.password">Password</label>
-              <Input
-                id="login.password"
-                type="password"
-                size="small"
-                name="login.password"
-                value={login.password}
-                onChange={this.handleInput}
-                placeholder="Your password"
-              />
-            </Form.Field>
-            <section className="Account__Submit-Bar">
-              <Form.Button primary>Login</Form.Button>
-              or
-              <Button basic>Register</Button>
-            </section>
-          </Form>
+          <Login />
         </Popup>
       </div>
     );
