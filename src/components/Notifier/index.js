5a2e4fbc6a4cbcd5f7cee9cec162b12719bf3422
Gaya Kessler <gaya.kessler@gmail.com>
2018-05-31 08:12:36 +0200

    display notification when update is available

--- src/components/Notifier/index.js
+++ src/components/Notifier/index.js
@@ -1,22 +1,32 @@
 // @flow
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Message from 'semantic-ui-react/dist/commonjs/collections/Message';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon';
 
 import './style.css';
 
-class Notifier extends Component<*> {
+type NotifierType = {
+  updateAvailable: boolean;
+}
+
+class Notifier extends Component<NotifierType> {
   reloadWindow = () => window.location.reload();
 
   render() {
+    const { updateAvailable } = this.props;
+
+    if (!updateAvailable) {
+      return null;
+    }
+
     return (
       <div className="Notifier">
         <Message
           compact
           info
-          className="clickable"
           onClick={this.reloadWindow}
         >
           <Icon name="refresh" />
@@ -27,4 +37,10 @@ class Notifier extends Component<*> {
   }
 }
 
-export default Notifier;
+function mapStateToProps(state) {
+  return {
+    updateAvailable: state.app.update,
+  };
+}
+
+export default connect(mapStateToProps)(Notifier);
