de81e40035bab67c6d40b96579b3907e2bf7158c
Gaya Kessler <gaya.kessler@gmail.com>
2018-06-06 10:32:46 +0200

    add status component

--- src/components/Account/api.js
+++ src/components/Account/api.js
@@ -5,3 +5,7 @@ import { post } from '../../core/fetch';
 export function registerUser(email: string, password: string): Promise<string> {
   return post('/register', { email, password });
 }
+
+export function refreshToken(): Promise<string> {
+  return post('/refresh-token');
+}
--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,13 +1,19 @@
 // @flow
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 import Popup from 'semantic-ui-react/dist/commonjs/modules/Popup';
 
+import Status from './Status';
 import Login from './Login';
 import Register from './Register';
 
 import './style.css';
+
+type AccountProps = {
+  jwt: string | null,
+}
 
 type AccountState = {
   isOpen: boolean,
@@ -21,7 +27,7 @@ function preventDefault(cb: (e: Event) => void) {
   };
 }
 
-class Account extends Component<*, AccountState> {
+class Account extends Component<AccountProps, AccountState> {
   state = {
     isOpen: false,
     view: 'login',
@@ -33,32 +39,42 @@ class Account extends Component<*, AccountState> {
   goToRegister = preventDefault(() => this.setState({ view: 'register' }));
 
   render() {
+    const { jwt } = this.props;
     const { isOpen, view } = this.state;
 
     return (
       <div>
-        <Popup
-          trigger={<Button inverted>Log in to sync</Button>}
-          open={isOpen}
-          position="bottom right"
-          on="click"
-          onClose={this.handleClose}
-          onOpen={this.handleOpen}
-        >
-          <div className="Account-PopUp">
-            <Login
-              inView={view === 'login'}
-              goToRegister={this.goToRegister}
-            />
-            <Register
-              inView={view === 'register'}
-              goToLogin={this.goToLogin}
-            />
-          </div>
-        </Popup>
+        {jwt && <Status />}
+        {!jwt && (
+          <Popup
+            trigger={<Button inverted>Log in to sync</Button>}
+            open={isOpen}
+            position="bottom right"
+            on="click"
+            onClose={this.handleClose}
+            onOpen={this.handleOpen}
+          >
+            <div className="Account-PopUp">
+              <Login
+                inView={view === 'login'}
+                goToRegister={this.goToRegister}
+              />
+              <Register
+                inView={view === 'register'}
+                goToLogin={this.goToLogin}
+              />
+            </div>
+          </Popup>
+        )}
       </div>
     );
   }
 }
 
-export default Account;
+function mapStateToProps(state) {
+  return {
+    jwt: state.account.jwt,
+  };
+}
+
+export default connect(mapStateToProps)(Account);
