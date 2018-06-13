da97db786b11b866e9d7f8c5408a25eea0138c54
Gaya Kessler <gaya.kessler@gmail.com>
2018-06-13 14:30:14 +0200

    fix types of export

--- src/core/importExport.js
+++ src/core/importExport.js
@@ -2,28 +2,13 @@
 
 import isBefore from 'date-fns/is_before';
 
-type exportStateType = {
-  time: {
-    allIds: Array<string>,
-    byId: {},
-  },
-  projects: {
-    allIds: Array<string>,
-    byId: {},
-  },
-  reports: {
-    allIds: Array<string>,
-    byId: {},
-  },
-};
-
 type exportType = {
   time: Array<timeType>,
-  projects: Array<timeType>,
-  reports: Array<timeType>,
+  projects: Array<projectType>,
+  reports: Array<reportType>,
 };
 
-export function stateToExport({ time, projects, reports }: exportStateType): exportType {
+export function stateToExport({ time, projects, reports }: storeShape): exportType {
   return {
     time: time.allIds.map(id => time.byId[id]),
     projects: projects.allIds.map(id => projects.byId[id]),
