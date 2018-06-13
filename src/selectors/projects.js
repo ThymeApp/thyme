952aff6ce5ab53aff06fb6240a2e868ae7cb41bf
Gaya Kessler <gaya.kessler@gmail.com>
2018-06-13 17:02:22 +0200

    filter removed projects from project selector

--- src/selectors/projects.js
+++ src/selectors/projects.js
@@ -5,7 +5,10 @@ import { createSelector } from 'reselect';
 import { sortProjects } from '../core/projects';
 
 export const getAllProjects =
-  (state: storeShape): projectType[] => state.projects.allIds.map(id => state.projects.byId[id]);
+  (state: storeShape): projectType[] =>
+    state.projects.allIds
+      .map(id => state.projects.byId[id])
+      .filter(project => !project.removed);
 
 export const sortedProjects = createSelector(
   getAllProjects,
