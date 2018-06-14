e8c6e1a97073a8b416cfa9a26a776ed7560a11da
Gaya Kessler <gaya.kessler@gmail.com>
2018-06-14 13:28:06 +0200

    fix flow type format

--- flow-typed/thymeLibDef.js
+++ flow-typed/thymeLibDef.js
@@ -50,38 +50,38 @@ declare type projectTreeType = projectType & { nameTree: Array<string> };
 declare type projectTreeWithTimeType = projectTreeType & { time: number, entries: Array<timeType> };
 
 declare type timePropertyType = {
-  project: string | null,
-  start: Date,
-  end: Date,
-  notes: string,
+  project: string | null;
+  start: Date;
+  end: Date;
+  notes: string;
 }
 
 declare type tempTimePropertyType = {
-  tracking: boolean,
+  tracking: boolean;
 } & timePropertyType;
 
 declare type timeType = {
-  id: string,
-  removed?: boolean,
-  createdAt: string,
-  updatedAt: string,
+  id: string;
+  removed?: boolean;
+  createdAt: string;
+  updatedAt: string;
 } & timePropertyType;
 
 declare type dateRanges = 'today' | 'week' | 'weekToDate' | 'month' | 'older';
 
 declare type RouterLocation = {
-  pathname: string,
-  search: string,
-  hash: string,
-  state?: any,
-  key?: string
+  pathname: string;
+  search: string;
+  hash: string;
+  state?: any;
+  key?: string;
 };
 
 declare type RouterMatch = {
-  isExact: boolean,
-  params: any,
-  path: string,
-  url: string,
+  isExact: boolean;
+  params: any;
+  path: string;
+  url: string;
 };
 
 declare type RouterHistory = {
