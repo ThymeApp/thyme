7372c232ab595b00c83fcbebe52926c03c8cf29a
Gaya Kessler <gaya.kessler@gmail.com>
2018-07-06 13:45:24 +0200

    remove unused actions from reports

--- src/actions/reports.js
+++ src/actions/reports.js
@@ -2,28 +2,6 @@
 
 import shortid from 'shortid';
 
-export function resetFilters(filters: Array<string>) {
-  return {
-    type: 'RESET_FILTERS',
-    filters,
-  };
-}
-
-export function toggleFilter(filter: string | null) {
-  return {
-    type: 'TOGGLE_FILTER',
-    filter,
-  };
-}
-
-export function updateDateRange(from: Date, to: Date) {
-  return {
-    type: 'UPDATE_DATE_RANGE',
-    from,
-    to,
-  };
-}
-
 export function addReport(name: string, filters: Array<string>, from: Date, to: Date) {
   return {
     type: 'ADD_REPORT',
