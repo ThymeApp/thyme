5f04c5573740457ef0615bb82e1426fb99b34272
Gaya Kessler <gaya.kessler@gmail.com>
2018-08-16 11:59:45 +0200

    test adding projects from dropdown

--- src/__tests__/integration/time.spec.js
+++ src/__tests__/integration/time.spec.js
@@ -99,18 +99,25 @@ describe('DateRange', () => {
 });
 
 describe('ThymeTable', () => {
-  const store = createStore();
-  const page = mount(
-    <Provider store={store}>
-      <Time now={parse('2018-08-14T19:00:00.000Z')} />
-    </Provider>,
-  );
-
   it('Has the correct starting date', () => {
+    const store = createStore();
+    const page = mount(
+      <Provider store={store}>
+        <Time now={parse('2018-08-14T19:00:00.000Z')} />
+      </Provider>,
+    );
+
     expect(page.find('ThymeTable').find('DateInput').prop('value')).toBe('2018-08-14');
   });
 
   it('Renders duration when times changes', () => {
+    const store = createStore();
+    const page = mount(
+      <Provider store={store}>
+        <Time now={parse('2018-08-14T19:00:00.000Z')} />
+      </Provider>,
+    );
+
     // change start time at 01:00 and end time to 03:00
     const startTime = document.createElement('input');
     startTime.value = '01:00';
@@ -125,6 +132,13 @@ describe('ThymeTable', () => {
   });
 
   it('Adds and updates entry to the store', () => {
+    const store = createStore();
+    const page = mount(
+      <Provider store={store}>
+        <Time now={parse('2018-08-14T19:00:00.000Z')} />
+      </Provider>,
+    );
+
     const startTime = document.createElement('input');
     startTime.value = '09:00';
 
@@ -160,11 +174,66 @@ describe('ThymeTable', () => {
     expect(updatedEntries[0].notes).toBe('Updated notes');
   });
 
-  it('Adds entry to the store with a project', () => {
+  it('Adds entry to the store with a project selected', () => {
+    const store = createStore({
+      projects: {
+        allIds: [1],
+        byId: {
+          1: {
+            id: 1,
+            name: 'Project name',
+          },
+        },
+      },
+    });
+    const page = mount(
+      <Provider store={store}>
+        <Time now={parse('2018-08-14T19:00:00.000Z')} />
+      </Provider>,
+    );
 
+    const startTime = document.createElement('input');
+    startTime.value = '09:00';
+
+    const endTime = document.createElement('input');
+    endTime.value = '12:00';
+
+    // set times
+    page.find('ThymeTable').find('input[type="time"]').at(0).simulate('change', { target: startTime });
+    page.find('ThymeTable').find('input[type="time"]').at(1).simulate('change', { target: endTime });
+
+    // select project
+    page.find('ThymeTable').find('input.search').simulate('click');
+    page.find('ThymeTable').find('.dropdown .item').at(1).simulate('click');
+
+    // submit
+    page.find('ThymeTable').find('.EntrySubmit').at(0).simulate('click');
+
+    const state = store.getState();
+    const entries = state.time.allIds.map(id => state.time.byId[id]);
+
+    expect(entries[0].project).toBe(1);
   });
 
   it('Creates project from project select', () => {
+    const store = createStore();
+    const page = mount(
+      <Provider store={store}>
+        <Time now={parse('2018-08-14T19:00:00.000Z')} />
+      </Provider>,
+    );
 
+    const projectName = document.createElement('input');
+    projectName.value = 'Test project';
+
+    // select project
+    page.find('ThymeTable').find('input.search').simulate('change', { target: projectName });
+    page.find('ThymeTable').find('.dropdown .item').at(0).simulate('click');
+
+    const state = store.getState();
+    const projects = state.projects.allIds.map(id => state.projects.byId[id]);
+
+    expect(projects.length).toBe(1);
+    expect(projects[0].name).toBe('Test project');
   });
 });
