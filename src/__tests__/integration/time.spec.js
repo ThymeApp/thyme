7d0919d5176f246f166eb86efb6229c1e57946c3
Gaya Kessler <gaya.kessler@gmail.com>
2018-08-16 11:05:53 +0200

    test updating notes

--- src/__tests__/integration/time.spec.js
+++ src/__tests__/integration/time.spec.js
@@ -105,10 +105,9 @@ describe('ThymeTable', () => {
       <Time now={parse('2018-08-14T19:00:00.000Z')} />
     </Provider>,
   );
-  const timeTableWrapper = page.find('ThymeTable');
 
   it('Has the correct starting date', () => {
-    expect(timeTableWrapper.find('DateInput').prop('value')).toBe('2018-08-14');
+    expect(page.find('ThymeTable').find('DateInput').prop('value')).toBe('2018-08-14');
   });
 
   it('Renders duration when times changes', () => {
@@ -119,9 +118,53 @@ describe('ThymeTable', () => {
     const endTime = document.createElement('input');
     endTime.value = '03:00';
 
-    timeTableWrapper.find('input[type="time"]').at(0).simulate('change', { target: startTime });
-    timeTableWrapper.find('input[type="time"]').at(1).simulate('change', { target: endTime });
+    page.find('ThymeTable').find('input[type="time"]').at(0).simulate('change', { target: startTime });
+    page.find('ThymeTable').find('input[type="time"]').at(1).simulate('change', { target: endTime });
 
-    expect(timeTableWrapper.find('.EntryDuration').at(0).text()).toBe('02:00');
+    expect(page.find('ThymeTable').find('.EntryDuration').at(0).text()).toBe('02:00');
+  });
+
+  it('Adds and updates entry to the store', () => {
+    const startTime = document.createElement('input');
+    startTime.value = '09:00';
+
+    const endTime = document.createElement('input');
+    endTime.value = '12:00';
+
+    const notes = document.createElement('input');
+    notes.value = 'Testing Thyme';
+
+    page.find('ThymeTable').find('input[type="time"]').at(0).simulate('change', { target: startTime });
+    page.find('ThymeTable').find('input[type="time"]').at(1).simulate('change', { target: endTime });
+    page.find('ThymeTable').find('.EntryNotes input').at(0).simulate('change', { target: notes });
+
+    page.find('ThymeTable').find('.EntrySubmit').at(0).simulate('click');
+
+    const state = store.getState();
+    const entries = state.time.allIds.map(id => state.time.byId[id]);
+
+    // see if entry has entered
+    expect(entries.length).toBe(1);
+    expect(entries[0].notes).toBe('Testing Thyme');
+
+    // testing updates
+    notes.value = 'Updated notes';
+
+    const notesInput = page.find('ThymeTable').find('.EntryNotes input').at(0);
+    notesInput.simulate('change', { target: notes });
+    notesInput.simulate('blur');
+
+    const updatedState = store.getState();
+    const updatedEntries = updatedState.time.allIds.map(id => updatedState.time.byId[id]);
+
+    expect(updatedEntries[0].notes).toBe('Updated notes');
+  });
+
+  it('Adds entry to the store with a project', () => {
+
+  });
+
+  it('Creates project from project select', () => {
+
   });
 });
