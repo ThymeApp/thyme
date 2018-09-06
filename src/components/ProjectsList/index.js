d9e779a4595d47beff0b4a5344093e11b43644af
Gaya Kessler <gaya.kessler@gmail.com>
2018-09-06 15:16:53 +0200

    fix table layout for project list

--- src/components/ProjectsList/index.js
+++ src/components/ProjectsList/index.js
@@ -1,23 +1,27 @@
 // @flow
 
-import React from 'react';
+import React, { Fragment } from 'react';
+
 import Table from 'semantic-ui-react/dist/commonjs/collections/Table';
+import Responsive from 'semantic-ui-react/dist/commonjs/addons/Responsive/Responsive';
 
 import ProjectsList from './ProjectsList';
 
 function projectsListWrapper({ projects }: { projects: Array<projectTreeType> }) {
   return (
     <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>
-            Name
-          </Table.HeaderCell>
-          <Table.HeaderCell colSpan={2}>
-            Parent
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+      <Responsive as={Fragment} minWidth={Responsive.onlyTablet.minWidth}>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>
+              Name
+            </Table.HeaderCell>
+            <Table.HeaderCell colSpan={2} width={6}>
+              Parent
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+      </Responsive>
       <Table.Body>
         <ProjectsList projects={projects} />
       </Table.Body>
