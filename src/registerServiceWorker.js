46e291ab4aec2a385a90d7e807e8ef9a38fb78c5
Gaya Kessler <gaya.kessler@gmail.com>
2018-09-12 07:29:53 +0200

    add slash if on thyme site

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -38,9 +38,7 @@ export default function register(storeDispatch, basename) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${basename}/service-worker.js`;
-
-      console.log(swUrl);
+      const swUrl = `${basename !== '' ? '/' : ''}${basename}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
