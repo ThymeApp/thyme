be8f0c6fb390b2e4b7a82b15f5df84d4aed668ae
Gaya Kessler <gaya.kessler@gmail.com>
2018-10-03 19:04:31 +0200

    code restructuring and some layout changes

--- src/components/ReportDetailed/index.js
+++ src/components/ReportDetailed/index.js
@@ -2,13 +2,13 @@
 
 import React, { Component } from 'react';
 import format from 'date-fns/format';
+import classnames from 'classnames';
 
 import Table from 'semantic-ui-react/dist/commonjs/collections/Table';
 import Accordion from 'semantic-ui-react/dist/commonjs/modules/Accordion';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon';
 import Message from 'semantic-ui-react/dist/commonjs/collections/Message';
 import Checkbox from 'semantic-ui-react/dist/commonjs/modules/Checkbox';
-import classnames from 'classnames';
 
 import { timeElapsed } from '../../core/thyme';
 
@@ -29,31 +29,32 @@ type ReportDetailedState = {
     duration: boolean,
     project: boolean,
     notes: boolean,
-  }, 
+  },
 };
 
 class ReportDetailed extends Component<ReportDetailedType, ReportDetailedState> {
-  
-  constructor () {
-    super();
-    this.onChangePrintView = this.onChangePrintView.bind(this);
-  }
-
   state = {
     opened: false,
-    printable: {date: true, start: true, end: true, duration: true, project: true, notes: true }
+    printable: {
+      date: true,
+      start: true,
+      end: true,
+      duration: true,
+      project: true,
+      notes: true,
+    },
   };
 
   onChangePrintView = (e: Event, data: { checked: boolean, column: string }) => {
     const { printable } = this.state;
     this.setState({
-      printable:  {
-        ...printable,   
-        [data.column]: data.checked 
-      }
+      printable: {
+        ...printable,
+        [data.column]: data.checked,
+      },
     });
-}
-  
+  };
+
   toggleDetails = () => {
     const { opened } = this.state;
 
@@ -64,61 +65,81 @@ class ReportDetailed extends Component<ReportDetailedType, ReportDetailedState> 
 
   render() {
     const { round, roundAmount, projects } = this.props;
-    const { opened } = this.state;
-    const { printable } = this.state; 
-    const {date, start, end, duration, project, notes} = printable; 
+    const { opened, printable } = this.state;
+
     return (
       <Accordion className="ReportDetailed">
         <Accordion.Title active={opened} onClick={this.toggleDetails}>
           <Icon name="dropdown" />
-          Detailed view - 
+          Detailed view
         </Accordion.Title>
         <Accordion.Content active={opened}>
-        <Message info>
-          <p>Select Columns to be in the print view</p>
-        </Message>
           <Table celled>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell className={classnames({'no-print': date})}>
+                <Table.HeaderCell className={classnames({ 'no-print': !printable.date })}>
                   Date
-                  <Checkbox  toggle defaultChecked  column="date" onClick={this.onChangePrintView}/> 
+                  <Checkbox
+                    checked={printable.date}
+                    column="date"
+                    onClick={this.onChangePrintView}
+                  />
                 </Table.HeaderCell>
-                <Table.HeaderCell className={classnames({'no-print': start})}>
+                <Table.HeaderCell className={classnames({ 'no-print': !printable.start })}>
                   Start
-                  <Checkbox toggle defaultChecked column="start" onClick={this.onChangePrintView}/> 
+                  <Checkbox
+                    checked={printable.start}
+                    column="start"
+                    onChange={this.onChangePrintView}
+                  />
                 </Table.HeaderCell>
-                <Table.HeaderCell className={classnames({'no-print': end})}>
+                <Table.HeaderCell className={classnames({ 'no-print': !printable.end })}>
                   End
-                  <Checkbox toggle defaultChecked column="end" onClick={this.onChangePrintView}/> 
+                  <Checkbox
+                    checked={printable.end}
+                    column="end"
+                    onChange={this.onChangePrintView}
+                  />
                 </Table.HeaderCell>
-                <Table.HeaderCell className={classnames({'no-print': duration})}>
+                <Table.HeaderCell className={classnames({ 'no-print': !printable.duration })}>
                   Duration
-                  <Checkbox toggle defaultChecked column="duration" onClick={this.onChangePrintView}/> 
+                  <Checkbox
+                    checked={printable.duration}
+                    column="duration"
+                    onChange={this.onChangePrintView}
+                  />
                 </Table.HeaderCell>
-                <Table.HeaderCell  className={classnames({'no-print': project})}>
+                <Table.HeaderCell className={classnames({ 'no-print': !printable.project })}>
                   Project
-                  <Checkbox toggle  defaultChecked column="project" onClick={this.onChangePrintView}/> 
+                  <Checkbox
+                    checked={printable.project}
+                    column="project"
+                    onChange={this.onChangePrintView}
+                  />
                 </Table.HeaderCell>
-                <Table.HeaderCell className={classnames({'no-print': notes})}>
+                <Table.HeaderCell className={classnames({ 'no-print': !printable.notes })}>
                   Notes
-                  <Checkbox toggle defaultChecked column="notes" onClick={this.onChangePrintView}/> 
+                  <Checkbox
+                    checked={printable.notes}
+                    column="notes"
+                    onChange={this.onChangePrintView}
+                  />
                 </Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
               {projects.map(project => project.entries.map(entry => (
                 <Table.Row key={entry.id}>
-                  <Table.Cell className={classnames({'no-print': date})}>
+                  <Table.Cell className={classnames({ 'no-print': !printable.date })}>
                     {format(entry.start, 'DD/MM/YYYY')}
                   </Table.Cell>
-                  <Table.Cell className={classnames({'no-print': start})}>
+                  <Table.Cell className={classnames({ 'no-print': !printable.start })}>
                     {format(entry.start, 'HH:mm')}
                   </Table.Cell>
-                  <Table.Cell className={classnames({'no-print': end})}>
+                  <Table.Cell className={classnames({ 'no-print': !printable.end })}>
                     {format(entry.end, 'HH:mm')}
                   </Table.Cell>
-                  <Table.Cell className={classnames({'no-print': duration})}>
+                  <Table.Cell className={classnames({ 'no-print': !printable.duration })}>
                     {timeElapsed(
                       entry.start,
                       entry.end,
@@ -128,16 +149,19 @@ class ReportDetailed extends Component<ReportDetailedType, ReportDetailedState> 
                       roundAmount,
                     )}
                   </Table.Cell>
-                  <Table.Cell className={classnames({'no-print': project})}>
+                  <Table.Cell className={classnames({ 'no-print': !printable.project })}>
                     {project.nameTree.join(' > ')}
                   </Table.Cell>
-                  <Table.Cell className={classnames({'no-print': notes})}>
+                  <Table.Cell className={classnames({ 'no-print': !printable.notes })}>
                     {entry.notes}
                   </Table.Cell>
                 </Table.Row>
               )))}
             </Table.Body>
           </Table>
+          <Message warning>
+            Checked columns will be visible in the print version of this page
+          </Message>
         </Accordion.Content>
       </Accordion>
     );
