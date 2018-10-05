725046b87a267fb84a284a9f444f98dcc283b0a3
Gaya Kessler <gaya.kessler@gmail.com>
2018-10-05 15:44:02 +0200

    move reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,12 @@ import { reducer as formReducer } from 'redux-form';
 
 import reports from 'sections/Reports/reducers';
 import settings from 'sections/Settings/reducers';
+import projects from 'sections/Projects/reducers';
 
 import runMigrations from '../migrations';
 
 import account from './account';
 import app from './app';
-import projects from './projects';
 import time from './time';
 
 const combinedReducers = combineReducers({
