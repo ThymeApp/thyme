df152f4c261a5fd35a6c6d69f389db70677cf6ca
Gaya Kessler <gaya.kessler@gmail.com>
2018-10-25 14:56:44 +0200

    move Status component to own folder

--- src/sections/Account/components/Status/index.js
+++ src/sections/Account/components/Status/index.js
@@ -18,11 +18,11 @@ import type { importDataType } from 'actions/app';
 import { getDataToExport } from 'selectors/importExport';
 import { isSyncing } from 'selectors/app';
 
-import { logout, updateToken } from '../actions';
+import { logout, updateToken } from '../../actions';
 
-import { getJwt } from '../selectors';
+import { getJwt } from '../../selectors';
 
-import { refreshToken, getState } from '../api';
+import { refreshToken, getState } from '../../api';
 
 import './Status.css';
 
