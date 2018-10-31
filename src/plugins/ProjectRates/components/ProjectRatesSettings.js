29e554c6f2f01f1294144c2f43f8416a5183f5ca
Gaya Kessler <gaya.kessler@gmail.com>
2018-10-31 09:15:11 +0100

    ability to add settings panels

---
+++ src/plugins/ProjectRates/components/ProjectRatesSettings.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React, { Component } from 'react';
+
+class ProjectRatesSettings extends Component<*> {
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default () => <ProjectRatesSettings />;
--- src/plugins/ProjectRates/injectables.js
+++ src/plugins/ProjectRates/injectables.js
@@ -2,10 +2,20 @@
 
 import { registerInjectable } from 'core/injectableComponent';
 
+import { registerSettingsItem } from 'sections/Settings/Settings';
+
 import ProjectTableHeader from './components/ProjectTableHeader';
 import ProjectHourlyRate from './components/ProjectHourlyRate';
+import ProjectRatesSettings from './components/ProjectRatesSettings';
 
 export default () => {
+  // Projects page
   registerInjectable('projects.tableheader.parent', 'projectRatesTableHeader', ProjectTableHeader);
   registerInjectable('projects.tablerow.parent', 'projectRatesRateInput', ProjectHourlyRate);
+
+  // Settings page
+  registerSettingsItem({
+    name: 'Project rates',
+    content: ProjectRatesSettings(),
+  });
 };
