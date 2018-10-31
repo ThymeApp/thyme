7f410eb13c9f4f6d03c1204b4414f1a5629173dd
Gaya Kessler <gaya.kessler@gmail.com>
2018-10-31 12:07:26 +0100

    add injectable components to reports

--- src/sections/Reports/components/Table/index.js
+++ src/sections/Reports/components/Table/index.js
@@ -5,12 +5,15 @@ import React from 'react';
 import Table from 'semantic-ui-react/dist/commonjs/collections/Table';
 
 import { formatDuration } from 'core/thyme';
+import { renderInjectable } from 'core/injectableComponent';
 
-type ReportTableType = {
+export type ReportTableProps = {
   projects: Array<projectTreeWithTimeType>,
 };
 
-function ReportTable({ projects }: ReportTableType) {
+function ReportTable(props: ReportTableProps) {
+  const { projects } = props;
+
   return (
     <Table celled unstackable>
       <Table.Header>
@@ -18,9 +21,11 @@ function ReportTable({ projects }: ReportTableType) {
           <Table.HeaderCell>
             Project
           </Table.HeaderCell>
+          {renderInjectable('reports.tableheader.project', props)}
           <Table.HeaderCell width={2} style={{ whiteSpace: 'nowrap' }}>
             Total spent
           </Table.HeaderCell>
+          {renderInjectable('reports.tableheader.total', props)}
         </Table.Row>
       </Table.Header>
       <Table.Body>
@@ -29,18 +34,22 @@ function ReportTable({ projects }: ReportTableType) {
             <Table.Cell>
               {project.nameTree.join(' > ')}
             </Table.Cell>
+            {renderInjectable('reports.tablerow.project', { project })}
             <Table.Cell textAlign="right">
               {formatDuration(project.time * 60)}
             </Table.Cell>
+            {renderInjectable('reports.tablerow.total', { project })}
           </Table.Row>
         ))}
       </Table.Body>
       <Table.Footer>
         <Table.Row>
           <Table.HeaderCell />
+          {renderInjectable('reports.tablefooter.project', props)}
           <Table.HeaderCell textAlign="right">
             {formatDuration(projects.reduce((total, project) => total + (project.time * 60), 0))}
           </Table.HeaderCell>
+          {renderInjectable('reports.tablefooter.total', props)}
         </Table.Row>
       </Table.Footer>
     </Table>
