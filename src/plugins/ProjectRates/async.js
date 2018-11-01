012a7d8de4816352f94487f36d817cd4b5a3f32a
Gaya Kessler <gaya.kessler@gmail.com>
2018-11-01 13:27:23 +0100

    dispatch plugin init when loaded

--- src/plugins/ProjectRates/async.js
+++ src/plugins/ProjectRates/async.js
@@ -1,9 +1,13 @@
 // @flow
 
+import { pluginInit } from 'actions/app';
+
 import registerReducers from './reducers';
 import registerInjectables from './injectables';
 
 export default function registerPlugin({ store }: { store: ThymeStore }) {
   registerInjectables();
   registerReducers(store);
+
+  store.dispatch(pluginInit('ProjectRates'));
 }
