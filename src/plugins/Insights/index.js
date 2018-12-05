dac3633b91d09bab393e3ed6ee44f6752babb087
Gaya Kessler <gaya.kessler@gmail.com>
2018-12-05 09:42:28 +0100

    remove debugger

--- src/plugins/Insights/index.js
+++ src/plugins/Insights/index.js
@@ -5,7 +5,5 @@ import { loadOnCapability } from 'register/plugin';
 import { canSeeInsights } from './selectors';
 
 export default function registerInsights(pluginInit: (name: string) => void) {
-  debugger;
-
   loadOnCapability(() => import('./async'), canSeeInsights, { pluginInit });
 }
--- src/plugins/ProjectRates/index.js
+++ src/plugins/ProjectRates/index.js
@@ -5,6 +5,5 @@ import { loadOnCapability } from 'register/plugin';
 import { canAddRates } from './selectors';
 
 export default function registerProjectRates(pluginInit: (name: string) => void) {
-  debugger;
   loadOnCapability(() => import('./async'), canAddRates, { pluginInit });
 }
