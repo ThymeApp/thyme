c53dfccb8b4da72920d3583df71347aa2e964696
Gaya Kessler <gaya.kessler@gmail.com>
2018-12-17 11:57:25 +0100

    correctly implement version compare

--- src/core/compareVersions.js
+++ src/core/compareVersions.js
@@ -1,14 +1,22 @@
 // @flow
 
 export default function isNewer(compare: string, version: string) {
+  if (compare === version) {
+    return false;
+  }
+
   const numbersCompare = compare.split('.').map(num => parseInt(num, 10));
   const numbersVersion = version.split('.').map(num => parseInt(num, 10));
 
-  return numbersCompare.every((num, index) => {
-    if (!numbersVersion[index]) {
+  for (let i = 0; i < numbersCompare.length; i += 1) {
+    if (!numbersVersion[i] || numbersCompare[i] > numbersVersion[i]) {
       return true;
     }
 
-    return num >= numbersVersion[index];
-  });
+    if (numbersCompare[i] < numbersVersion[i]) {
+      return false;
+    }
+  }
+
+  return false;
 }
