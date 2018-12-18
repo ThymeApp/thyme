9a5b786a24e34b043cb511553ba991cd45226d9c
Gaya Kessler <gaya.kessler@gmail.com>
2018-12-18 14:06:18 +0100

    remove comment

--- src/selectors/importExport.js
+++ src/selectors/importExport.js
@@ -6,8 +6,6 @@ const allTheTime = state => state.time;
 const allTheProjects = state => state.projects;
 const allTheReports = state => state.reports;
 
-// <StateShape, *, TimeType[], *>
-
 export const getDataToExport = createSelector<StateShape, *, ExportShape, *>(
   (state: StateShape) => [allTheTime(state), allTheProjects(state), allTheReports(state)],
   ([time, projects, reports]) => ({ time, projects, reports }),
