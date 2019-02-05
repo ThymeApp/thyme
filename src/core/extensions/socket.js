e88e24e080d196802bd363602eb5973e57bfd1d1
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-05 13:05:26 +0100

    communicate change from server

--- src/core/extensions/socket.js
+++ src/core/extensions/socket.js
@@ -5,7 +5,7 @@ import type { Socket } from 'socket.io-client';
 
 import { isLoggedIn, hasPremium } from 'sections/Account/selectors';
 
-import { onChangeTimer, onChangeState } from './events';
+import { onChangeTimer, onChangeState, receiveTimer } from './events';
 
 function canSync(state: StateShape): boolean {
   return state && isLoggedIn(state) && hasPremium(state);
@@ -20,22 +20,32 @@ function jwt(state: StateShape): string | null {
 }
 
 function startSocketConnection(socket: Socket) {
+  let communicatedConnection = false;
   let state: StateShape;
 
-  onChangeState((newState) => { state = newState; });
+  onChangeState((newState) => {
+    state = newState;
 
-  onChangeTimer((item) => {
-    if (canSync(state)) {
+    if (!communicatedConnection && canSync(state)) {
+      communicatedConnection = true;
+
       const token = jwt(state);
-      socket.emit('changeItem', {
-        token,
-        item,
-      });
+
+      socket.emit('connectUser', { token });
     }
   });
 
-  socket.on('connect', () => console.log('connected'));
-  socket.on('disconnect', () => console.log('disconnected'));
+  onChangeTimer((item) => {
+    if (canSync(state)) {
+      socket.emit('changeItem', { item });
+    }
+  });
+
+  socket.on('changeItem', (data) => {
+    if (data.socket !== socket.id) {
+      receiveTimer(data.item, false);
+    }
+  });
 }
 
 startSocketConnection(io(process.env.REACT_APP_API_ROOT));
