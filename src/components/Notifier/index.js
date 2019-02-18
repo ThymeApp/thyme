b898a183804e3f02c9867ae96c068e10bba625ef
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-18 16:45:57 +0100

    simplify notifier

--- src/components/Notifier/index.js
+++ src/components/Notifier/index.js
@@ -1,48 +1,33 @@
 // @flow
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
 import Message from 'semantic-ui-react/dist/commonjs/collections/Message';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon';
+
+import { useMappedState } from 'core/useRedux';
 
 import { updateAvailable } from 'selectors/app';
 
 import './style.css';
 
-type NotifierType = {
-  isUpdateAvailable: boolean;
-}
+const reloadWindow = () => window.location.reload();
 
-class Notifier extends Component<NotifierType> {
-  reloadWindow = () => window.location.reload();
+function Notifier() {
+  const isUpdateAvailable = useMappedState(updateAvailable);
 
-  render() {
-    const { isUpdateAvailable } = this.props;
-
-    if (!isUpdateAvailable) {
-      return null;
-    }
-
-    return (
-      <div className="Notifier">
-        <Message
-          compact
-          info
-          onClick={this.reloadWindow}
-        >
-          <Icon name="refresh" />
-          New version available. Refresh the page to load.
-        </Message>
-      </div>
-    );
+  if (!isUpdateAvailable) {
+    return null;
   }
+
+  return (
+    <div className="Notifier">
+      <Message compact info onClick={reloadWindow}>
+        <Icon name="refresh" />
+        New version available. Refresh the page to load.
+      </Message>
+    </div>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    isUpdateAvailable: updateAvailable(state),
-  };
-}
-
-export default connect(mapStateToProps)(Notifier);
+export default Notifier;
