c020191194751d35d9554a065e7fa9aff5c4a7f8
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-20 15:53:36 +0100

    immediately check capabilities on plugin load with file is slower than api response

--- src/register/plugin.js
+++ src/register/plugin.js
@@ -4,30 +4,39 @@ import { ofType } from 'redux-observable';
 import { filter, mergeMap } from 'rxjs/operators';
 
 import { inject } from 'register/epic';
+import { getState } from 'register/reducer';
 
 export function loadOnCapability(
   importModule: () => Promise<*>,
   selector: (state: any) => boolean,
   args: any,
 ) {
-  const asyncEpic = (
-    action$: ActionsObservable,
-    state$: StateObservable,
-  ) => action$.pipe(
-    ofType('ACCOUNT_RECEIVE_INFORMATION'),
-    mergeMap((): any[] | Promise<*> => {
-      const matchesCapability = selector(state$.value);
+  const onLoadPlugin = () => importModule()
+    .then(module => module && typeof module.default === 'function' && module.default(args));
+  const initialMatchesCapability = selector(getState());
 
-      if (!matchesCapability) {
-        return [];
-      }
+  // if already possible, don't wait for receiving information
+  if (initialMatchesCapability) {
+    onLoadPlugin();
+  } else {
+    const asyncEpic = (
+      action$: ActionsObservable,
+      state$: StateObservable,
+    ) => action$.pipe(
+      ofType('ACCOUNT_RECEIVE_INFORMATION'),
+      mergeMap((): any[] | Promise<*> => {
+        const matchesCapability = selector(state$.value);
 
-      return importModule()
-        .then(module => module && typeof module.default === 'function' && module.default(args));
-    }),
-    // prevent new actions
-    filter(() => false),
-  );
+        if (!matchesCapability) {
+          return [];
+        }
 
-  inject(asyncEpic);
+        return onLoadPlugin();
+      }),
+      // prevent new actions
+      filter(() => false),
+    );
+
+    inject(asyncEpic);
+  }
 }
