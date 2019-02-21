d5858093b48bcabedb751f6a6a3b65a7c46de855
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-21 11:44:55 +0100

    use project colours on charts

--- src/sections/Reports/components/Charts/index.js
+++ src/sections/Reports/components/Charts/index.js
@@ -5,34 +5,13 @@ import PieChart from 'react-svg-piechart';
 
 import { treeDisplayName } from 'core/projects';
 
+import { colourMap, colours } from 'sections/Projects/consts';
+
 import './ReportCharts.css';
 
 type ReportChartsType = {
   projects: Array<ProjectTreeWithTimeType>,
 };
-
-export const colours = [
-  '#00AA55',
-  '#009FD4',
-  '#B381B3',
-  '#EF4836',
-  '#AA8F00',
-  '#D47500',
-  '#939393',
-  '#9966cc',
-  '#008000',
-  '#e9d66b',
-  '#a52a2a',
-  '#6e7f80',
-  '#007fff',
-  '#f4c2c2',
-  '#3d2b1f',
-  '#79443b',
-  '#66ff00',
-  '#1dacd6',
-  '#b5a642',
-  '#08e8de',
-];
 
 function ReportCharts({ projects }: ReportChartsType) {
   const projectsWithTime = projects.filter(project => project.time > 0);
@@ -54,7 +33,7 @@ function ReportCharts({ projects }: ReportChartsType) {
           data={projectsWithTime.map((project, index) => ({
             title: treeDisplayName(project),
             value: Math.round(project.time),
-            color: colours[index % 20],
+            color: colourMap[project.colour || colours[index % colours.length]],
           }))}
         />
       </div>
@@ -63,7 +42,7 @@ function ReportCharts({ projects }: ReportChartsType) {
           <li key={project.id}>
             <span
               className="ReportCharts__Legend-Colour"
-              style={{ borderColor: colours[index] }}
+              style={{ borderColor: colourMap[project.colour || colours[index % colours.length]] }}
             />
             {treeDisplayName(project)}
           </li>
