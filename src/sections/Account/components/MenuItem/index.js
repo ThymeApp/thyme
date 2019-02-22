9c60b2f6039031782fc1cceefa4506c03b99e245
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-22 10:37:08 +0100

    show last synced text in menu

--- src/sections/Account/components/MenuItem/index.js
+++ src/sections/Account/components/MenuItem/index.js
@@ -1,15 +1,19 @@
 // @flow
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import React, { useState, useCallback } from 'react';
 import { withRouter } from 'react-router';
 import type { RouterHistory } from 'react-router';
+
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon';
 import Popup from 'semantic-ui-react/dist/commonjs/modules/Popup';
 import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu';
+
+import { useMappedState, useDispatch } from 'core/useRedux';
+
+import { getLastSync } from 'selectors/app';
 
 import { logout } from '../../actions';
 
@@ -22,117 +26,90 @@ import './MenuItem.css';
 
 type AccountProps = {
   history: RouterHistory;
-  onLogout: () => void;
-  loggedIn: boolean;
-  showPremium: boolean;
 }
 
-type AccountState = {
-  isOpen: boolean;
-}
+function Account({ history }: AccountProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-class Account extends Component<AccountProps, AccountState> {
-  state = {
-    isOpen: false,
-  };
-
-  onLogout = () => {
-    const { onLogout } = this.props;
-
-    onLogout();
-  };
-
-  handleOpen = () => this.setState({ isOpen: true });
-
-  handleClose = () => this.setState({ isOpen: false });
-
-  goToSettings = () => {
-    const { history } = this.props;
-
-    history.push('/account');
-    this.handleClose();
-  };
-
-  goToPremium = () => {
-    const { history } = this.props;
-
-    history.push('/premium');
-    this.handleClose();
-  };
-
-  render() {
-    const { loggedIn, showPremium } = this.props;
-    const { isOpen } = this.state;
-
-    return (
-      <Popup
-        className="Account-PopUp"
-        trigger={(
-          <Button
-            secondary={loggedIn}
-            inverted={!loggedIn}
-          >
-            { !loggedIn && <Icon name="unlock" /> }
-            { loggedIn ? <Status closePopup={this.handleClose} /> : 'Login' }
-          </Button>
-        )}
-        open={isOpen}
-        position="bottom right"
-        on="click"
-        onClose={this.handleClose}
-        onOpen={this.handleOpen}
-      >
-        { loggedIn ? (
-          <Menu vertical>
-            {showPremium && (
-              <Menu.Item name="premium" className="Account-BackUp">
-                Syncing is disabled without premium subscription.
-
-                <Button primary fluid onClick={this.goToPremium}>
-                  <Icon name="diamond" />
-                  Buy Premium
-                </Button>
-              </Menu.Item>
-            )}
-            <Menu.Item
-              name="settings"
-              onClick={this.goToSettings}
-            >
-              Account settings
-            </Menu.Item>
-            <Menu.Item
-              name="logout"
-              onClick={this.onLogout}
-            >
-              Logout
-            </Menu.Item>
-          </Menu>
-        ) : (
-          <div className="Account-PopUp-Content">
-            <Login goToRegister={this.goToPremium} />
-          </div>
-        )}
-      </Popup>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
+  const { loggedIn, showPremium, lastSync } = useMappedState(state => ({
+    lastSync: getLastSync(state),
     loggedIn: isLoggedIn(state),
     showPremium: !hasPremium(state) && isLoaded(state),
-  };
+  }));
+
+  const onLogout = useDispatch(dispatch => () => dispatch(logout()));
+
+  const handleOpen = useCallback(() => setIsOpen(true), [setIsOpen]);
+  const handleClose = useCallback(() => setIsOpen(false), [setIsOpen]);
+
+  const goToSettings = useCallback(() => {
+    history.push('/account');
+    handleClose();
+  }, [history, handleClose]);
+
+  const goToPremium = useCallback(() => {
+    history.push('/premium');
+    handleClose();
+  }, [history, handleClose]);
+
+  return (
+    <Popup
+      className="Account-PopUp"
+      trigger={(
+        <Button
+          secondary={loggedIn}
+          inverted={!loggedIn}
+        >
+          { !loggedIn && <Icon name="unlock" /> }
+          { loggedIn ? <Status closePopup={handleClose} /> : 'Login' }
+        </Button>
+      )}
+      open={isOpen}
+      position="bottom right"
+      on="click"
+      onClose={handleClose}
+      onOpen={handleOpen}
+    >
+      { loggedIn ? (
+        <Menu vertical>
+          {showPremium && (
+            <Menu.Item name="premium" className="Account-BackUp">
+              Syncing is disabled without premium subscription.
+
+              <Button primary fluid onClick={goToPremium}>
+                <Icon name="diamond" />
+                Buy Premium
+              </Button>
+            </Menu.Item>
+          )}
+          <Menu.Item
+            name="last-sync"
+            style={{ lineHeight: '1.1em', backgroundColor: 'rgba(0, 0, 0, .02)' }}
+          >
+            <strong>Last sync: </strong>
+            {distanceInWordsToNow(lastSync, { includeSeconds: true })}
+            <span> ago</span>
+          </Menu.Item>
+          <Menu.Item
+            name="settings"
+            onClick={goToSettings}
+          >
+            Account settings
+          </Menu.Item>
+          <Menu.Item
+            name="logout"
+            onClick={onLogout}
+          >
+            Logout
+          </Menu.Item>
+        </Menu>
+      ) : (
+        <div className="Account-PopUp-Content">
+          <Login goToRegister={goToPremium} />
+        </div>
+      )}
+    </Popup>
+  );
 }
 
-function mapDispatchToProps(dispatch: ThymeDispatch) {
-  return {
-    onLogout() {
-      dispatch(logout());
-    },
-  };
-}
-
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  withRouter,
-)(Account);
+export default withRouter<*>(Account);
--- src/selectors/app.js
+++ src/selectors/app.js
@@ -1,5 +1,6 @@
 // @flow
 
 export const isSyncing = (state: StateShape) => state.app.syncing;
+export const getLastSync = (state: StateShape) => state.app.lastSync;
 export const getAlert = (state: StateShape) => state.app.alert;
 export const updateAvailable = (state: StateShape) => state.app.update;
