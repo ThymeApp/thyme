5ce76cbaf329f34b662c987f6f818c8b8ffff34c
Gaya Kessler <gaya.kessler@gmail.com>
2019-03-04 13:28:19 +0100

    add flow type

--- src/core/analytics.js
+++ src/core/analytics.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { useEffect } from 'react';
 
 const _paq = window._paq || []; // eslint-disable-line
@@ -5,14 +7,14 @@ const skipEnvs = ['development', 'test'];
 
 const enabled = skipEnvs.indexOf(process.env.NODE_ENV) === -1;
 
-export function trackPageview(pageName = '') {
+export function trackPageview(pageName: string = '') {
   if (enabled) _paq.push(['trackPageView', pageName]);
 }
 
-export function trackEvent(category = '', action = '') {
+export function trackEvent(category: string = '', action: string = '') {
   if (enabled) _paq.push(['trackEvent', category, action]);
 }
 
-export function useTrackPageview(pageName = '') {
+export function useTrackPageview(pageName: string = '') {
   useEffect(() => trackPageview(pageName), []);
 }
