8933f46e45ba56055a9f4e60e89c6e8f6019c27e
Gaya Kessler <gaya.kessler@gmail.com>
2019-04-24 09:57:59 +0200

    also check online version

--- scripts/check-versions.js
+++ scripts/check-versions.js
@@ -1,5 +1,10 @@
+const https = require('https');
+const semver = require('semver');
+
 const packageJson = require('../package.json');
 const packageLockJson = require('../package-lock.json');
+
+const githubPackageJsonLocation = 'https://raw.githubusercontent.com/ThymeApp/thyme/master/package.json';
 
 const errorMessage = `Package versions do not match.
 
@@ -12,3 +17,23 @@ if (packageJson.version !== packageLockJson.version) {
   console.error(errorMessage);
   process.exit(1);
 }
+
+https.get(githubPackageJsonLocation, (res) => {
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    const data = JSON.parse(rawData);
+
+    if (!semver.lt(data.version, packageJson.version)) {
+      console.error(`Published version ${data.version} is greater or equal to ${packageJson.version}.
+
+Increase version in package.json
+
+Checked against: ${githubPackageJsonLocation}
+`);
+      process.exit(1);
+    }
+  });
+}).on('error', (e) => {
+  console.error(e);
+});
