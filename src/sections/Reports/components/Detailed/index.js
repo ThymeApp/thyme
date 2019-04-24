5a21694d4f84dcd693a54c49259a4a5bf9c48f67
Gaya Kessler <gaya.kessler@gmail.com>
2019-04-24 14:04:53 +0200

    report details to hooks

--- src/sections/Reports/components/Detailed/index.js
+++ src/sections/Reports/components/Detailed/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import format from 'date-fns/format';
 
 import Checkbox from 'semantic-ui-react/dist/commonjs/modules/Checkbox';
@@ -12,126 +12,114 @@ import { create as createTable } from 'register/table';
 
 import './ReportDetailed.css';
 
-type ReportDetailedType = {
+type ReportDetailedProps = {
   enabledEndDate: boolean;
   round: Rounding;
   roundAmount: number;
   projects: ProjectTreeWithTimeType[];
 };
 
-type ReportDetailedState = {
-  opened: boolean;
-};
-
 const sortAsc = sortByTime('asc');
 
-class ReportDetailed extends Component<ReportDetailedType, ReportDetailedState> {
-  state = {
-    opened: false,
-  };
+function ReportDetailed({
+  round,
+  roundAmount,
+  projects,
+  enabledEndDate,
+}: ReportDetailedProps) {
+  const [isOpened, setIsOpened] = useState(false);
 
-  toggleDetails = (e: Event, data: { checked: boolean }) => {
-    this.setState({
-      opened: data.checked,
-    });
-  };
+  const toggleDetails = useCallback(
+    (e: Event, data: { checked: boolean }) => setIsOpened(data.checked),
+    [],
+  );
 
-  render() {
-    const {
-      round,
-      roundAmount,
-      projects,
-      enabledEndDate,
-    } = this.props;
-    const { opened } = this.state;
+  const rows = projects
+    .reduce((acc, project) => [
+      ...acc,
+      ...project.entries.map(entry => ({
+        ...entry,
+        project,
+      })),
+    ], [])
+    .sort(sortAsc);
 
-    const rows = projects
-      .reduce((acc, project) => [
-        ...acc,
-        ...project.entries.map(entry => ({
-          ...entry,
-          project,
-        })),
-      ], [])
-      .sort(sortAsc);
-
-    if (rows.length === 0) {
-      return null;
-    }
-
-    const reportTable = createTable(
-      'reports.detailed', [
-        {
-          name: enabledEndDate ? 'Start Date' : 'Date',
-          header: () => (enabledEndDate ? 'Start Date' : 'Date'),
-          row: entry => format(entry.start, 'DD/MM/YYYY'),
-          collapsing: true,
-        },
-        {
-          name: 'Start',
-          header: () => 'Start',
-          row: entry => format(entry.start, 'HH:mm'),
-          collapsing: true,
-        },
-        enabledEndDate ? {
-          name: 'End Date',
-          header: () => 'End Date',
-          row: entry => format(entry.end, 'DD/MM/YYYY'),
-          collapsing: true,
-        } : null,
-        {
-          name: 'End',
-          header: () => 'End',
-          row: entry => format(entry.end, 'HH:mm'),
-          collapsing: true,
-        },
-        {
-          name: 'Duration',
-          header: () => 'Duration',
-          row: entry => timeElapsed(
-            entry.start,
-            entry.end,
-            false,
-            false,
-            round,
-            roundAmount,
-          ),
-          textAlign: 'right',
-          collapsing: true,
-        },
-        {
-          name: 'Project',
-          header: () => 'Project',
-          row: entry => treeDisplayName(entry.project),
-          collapsing: true,
-        },
-        {
-          name: 'Notes',
-          header: () => 'Notes',
-          row: entry => entry.notes,
-        },
-      ],
-      rows,
-    );
-
-    return (
-      <section className="ReportDetailed">
-        <div className="ReportDetailed__toggle">
-          <Checkbox
-            label="Detailed view"
-            toggle
-            checked={opened}
-            onChange={this.toggleDetails}
-            style={opened ? { marginRight: '2em' } : {}}
-          />
-
-          {opened && reportTable.filters}
-        </div>
-
-        {opened && reportTable.table}
-      </section>
-    );
+  if (rows.length === 0) {
+    return null;
   }
+
+  const reportTable = createTable(
+    'reports.detailed', [
+      {
+        name: enabledEndDate ? 'Start Date' : 'Date',
+        header: () => (enabledEndDate ? 'Start Date' : 'Date'),
+        row: entry => format(entry.start, 'DD/MM/YYYY'),
+        collapsing: true,
+      },
+      {
+        name: 'Start',
+        header: () => 'Start',
+        row: entry => format(entry.start, 'HH:mm'),
+        collapsing: true,
+      },
+      enabledEndDate ? {
+        name: 'End Date',
+        header: () => 'End Date',
+        row: entry => format(entry.end, 'DD/MM/YYYY'),
+        collapsing: true,
+      } : null,
+      {
+        name: 'End',
+        header: () => 'End',
+        row: entry => format(entry.end, 'HH:mm'),
+        collapsing: true,
+      },
+      {
+        name: 'Duration',
+        header: () => 'Duration',
+        row: entry => timeElapsed(
+          entry.start,
+          entry.end,
+          false,
+          false,
+          round,
+          roundAmount,
+        ),
+        textAlign: 'right',
+        collapsing: true,
+      },
+      {
+        name: 'Project',
+        header: () => 'Project',
+        row: entry => treeDisplayName(entry.project),
+        collapsing: true,
+      },
+      {
+        name: 'Notes',
+        header: () => 'Notes',
+        row: entry => entry.notes,
+      },
+    ],
+    rows,
+  );
+
+  return (
+    <section className="ReportDetailed">
+      <div className="ReportDetailed__toggle">
+        <Checkbox
+          label="Detailed view"
+          toggle
+          checked={isOpened}
+          onChange={toggleDetails}
+          style={isOpened ? { marginRight: '2em' } : {}}
+        />
+
+        {isOpened && reportTable.filters}
+      </div>
+
+      {isOpened && reportTable.table}
+    </section>
+  );
 }
 
 export default ReportDetailed;
